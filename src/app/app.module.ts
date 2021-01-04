e036604c3e48c4c86e95b6c48483f52436186672
andresmauricio <andresacelas@outlook.com>
2021-01-04 17:29:38 -0500

    Componentes base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PadreComponent } from './components/padre/padre.component';
+import { HijoComponent } from './components/hijo/hijo.component';
+import { NietoComponent } from './components/nieto/nieto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PadreComponent,
+    HijoComponent,
+    NietoComponent
   ],
   imports: [
     BrowserModule,
