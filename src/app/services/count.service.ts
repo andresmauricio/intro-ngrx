7eed815bbc77d988dc6f00d688eb85b86b0c8a3f
andresmauricio <andresacelas@outlook.com>
2021-01-04 21:23:01 -0500

    Dispara acciones y suscribirse

--- src/app/services/count.service.ts
+++ src/app/services/count.service.ts
@@ -1,28 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import * as actionCounter from "../count.action";
 
 @Injectable({ providedIn: 'root' })
 export class CountService {
-  public count = 10 ;
+  public count$: Observable<number>;
 
-  constructor() {}
+  constructor(private store: Store<{count: number}>) {
+    this.count$ = store.select('count');
+  }
 
   incrementar(): void {
-    this.count += 1;
+    // this.count += 1;
+    this.store.dispatch(actionCounter.INCREMENT());
   }
 
   decrementar(): void {
-    this.count -= 1;
+    //this.count -= 1;
+    this.store.dispatch(actionCounter.DECREMENT());
   }
 
   multiplicar() {
-    this.count *= 2;
+    //this.count *= 2;
   }
 
   dividir() {
-    this.count /= 2;
+    //this.count /= 2;
   }
 
   reset(): void {
-    this.count = 0;
+    //this.count = 0;
   }
 }
